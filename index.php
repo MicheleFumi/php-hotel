9d48b087966765d56636fcb69073fca87da820fb
MicheleFumi <michele.fumi96@gmail.com>
2025-03-03 09:36:01 +0100

    bug fix

--- index.php
+++ index.php
@@ -1,138 +1,161 @@
-<!DOCTYPE html>
+<?php
+
+require_once './database.php';
+
+?>
+
+<!doctype html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <title>hotel</title>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
+    <!-- Bootstrap CSS v5.2.1 -->
+    <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+        crossorigin="anonymous" />
 </head>
 
 <body>
-    <?php
+    <header>
 
-    $hotels = [
+    </header>
+    <main>
 
-        [
-            'name' => 'Hotel Belvedere',
-            'description' => 'Hotel Belvedere Descrizione',
-            'parking' => true,
-            'vote' => 4,
-            'distance_to_center' => 10.4
-        ],
-        [
-            'name' => 'Hotel Futuro',
-            'description' => 'Hotel Futuro Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 2
-        ],
-        [
-            'name' => 'Hotel Rivamare',
-            'description' => 'Hotel Rivamare Descrizione',
-            'parking' => false,
-            'vote' => 1,
-            'distance_to_center' => 1
-        ],
-        [
-            'name' => 'Hotel Bellavista',
-            'description' => 'Hotel Bellavista Descrizione',
-            'parking' => false,
-            'vote' => 5,
-            'distance_to_center' => 5.5
-        ],
-        [
-            'name' => 'Hotel Milano',
-            'description' => 'Hotel Milano Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 50
-        ],
+        <div class="container">
+
+            <form action="./index.php" method="get" class="mt-4 w-50 mx-auto p-3 border rounded ">
 
 
-
-    ];
-
-    //var_dump($hotels)
-
-    ?>
-
-    <div class="container">
-        <h1 class="my-4">Choose the hotel</h1>
-
-        <div class="py-2  text-center">
-            <form action="" method="get" class="container p-4 border rounded shadow-sm bg-light w-50">
-
-                <!-- Input Number Vote -->
                 <div class="mb-3">
-                    <label for="vote" class="form-label">How many stars?</label>
-                    <input type="number" class="form-control" name="vote" id="vote" placeholder="how many stars?">
+                    <label for="rating" class="form-label">Quante stelle?</label>
+                    <input class="form-control" type="number" name="rating" id="rating" min="1" max="5" />
                 </div>
 
-                <!-- Checkbox Parking -->
-                <div class="mb-3 form-check">
-                    <input type="checkbox" class="form-check-input" name="parking" id="parking">
-                    <label class="form-check-label pe-4" for="parking">Want to park?</label>
+                <div class="mb-3">
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" name="parking" id="parking" />
+                        <label class="form-check-label" for="parking">Vuoi parcheggiare?</label>
+                    </div>
                 </div>
 
-                <!-- Submit Button -->
-                <div class="text-center">
-                    <button type="submit" class="btn btn-primary w-100">Search</button>
-                </div>
+
+                <button class="btn btn-primary" type="submit">Invia</button>
             </form>
 
 
 
-            <?php
-            $checkmark = $_GET["parking"];
-            $vote = $_GET["vote"];
-            ?>
-        </div>
+            <div class="title my-5 text-center">
+                <h2>tutti gli hotel</h2>
+            </div>
 
-        <h2>Your Results:</h2>
-        <hr>
-        <div class="mt-3">
             <?php
-            if (!$checkmark && !$vote) {
+            $filteredHotel = [];
+
+
+            if (!isset($_GET['parking']) && !isset($_GET['rating'])) {
+
                 foreach ($hotels as $hotel) {
-
-                    foreach ($hotel as $key => $value) {
-                        if ($key == "parking") {
-                            echo "parking: " . ($value ? "yes" : "no");
-                        } else {
-                            echo "$key : $value";
-                        };
-
-                        echo "<br>";
-                    }
-                    echo "<hr>";
+                    $filteredHotel[] = $hotel;
                 }
-            } elseif ($checkmark == "on" || $vote) {
+            } elseif (!isset($_GET['parking']) && $_GET['rating'] != '') {
                 foreach ($hotels as $hotel) {
-
-                    if ($hotel["parking"] && $hotel["vote"] >= $vote) {
-
-                        echo "
-                        name: $hotel[name]<br> 
-                        description: $hotel[description] <br>
-                        parking: yes <br>
-                        vote: $hotel[vote] <br>
-                        distance_to_center: $hotel[distance_to_center] <br>
-                        <hr>
-                        ";
+                    if ($hotel['vote'] >= $_GET['rating']) {
+                        $filteredHotel[] = $hotel;
+                    }
+                }
+            } elseif (isset($_GET['parking']) && $_GET['parking'] == 'on' && $_GET['rating'] == '') {
+                foreach ($hotels as $hotel) {
+                    if ($hotel['parking'] == true) {
+                        $filteredHotel[] = $hotel;
+                    }
+                }
+            } elseif (isset($_GET['parking']) && $_GET['parking'] == 'on' && $_GET['rating'] != '') {
+                foreach ($hotels as $hotel) {
+                    if ($hotel['parking'] == true && $hotel['vote'] >= $_GET['rating']) {
+                        $filteredHotel[] = $hotel;
                     }
                 }
             }
 
+            /* 1. controlla che tutto sia disattivato */
+            /* 2. controlla che solo rating sia inserito e filtra */
+            /* 3. controlla che solo il parcheggio sia inserito e filtra */
+            /* 4. controlla che sia il parcheggio che il rating siano inserito e filtra */
+
+
+
             ?>
+
+
+
+
+
+
+
+
+            <?php
+
+            foreach ($filteredHotel as $hotel) {
+            ?>
+                <div class="card my-2">
+                    <div class="card-header">
+                        <strong>nome hotel:</strong> <?php echo $hotel['name'] ?>
+                    </div>
+                    <div class="card-body">
+                        <div>
+                            <strong>descrizione:</strong> <?php echo $hotel['description'] ?>
+                        </div>
+                        <div>
+                            <strong>parcheggio:</strong>
+                            <?php
+
+                            echo $hotel['parking'] ? 'si' : 'no';
+
+                            ?>
+                        </div>
+                        <div>
+                            <strong> voto:</strong>
+                            <?php
+                            echo
+                            $hotel['vote']
+                            ?>
+                        </div>
+
+                        <div>
+                            <strong>distanza dal centro:</strong> <?php echo $hotel['distance_to_center'] ?>
+                        </div>
+                    </div>
+                </div>
+            <?php
+
+            }
+
+            ?>
+
+
         </div>
+    </main>
+    <footer>
 
+    </footer>
 
+    <script
+        src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+        crossorigin="anonymous"></script>
 
-
-    </div>
-
+    <script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+        integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+        crossorigin="anonymous"></script>
 </body>
 
 </html>
